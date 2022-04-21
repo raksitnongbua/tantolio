2c423cc981978f0de562c9c99574c649547a0137
Tan Raksit <tan.raksit@bitkub.com>
2022-04-21 18:10:06 +0700

    feat: update is()

--- apps/frontend/src/components/Contact.tsx
+++ apps/frontend/src/components/Contact.tsx
@@ -4,23 +4,23 @@ import ScrollAnimation from 'react-animate-on-scroll';
 const Contact = () => {
   return (
     <Box
-      minHeight="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
+      minHeight='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
     >
-      <ScrollAnimation animateIn="flipInX" animateOut="flipOutX">
-        <Box display="flex" my={1}>
-          <Typography variant="h3" color="textPrimary">
+      <ScrollAnimation animateIn='flipInX' animateOut='flipOutX'>
+        <Box display='flex' my={1}>
+          <Typography variant='h3' color='textPrimary'>
             contact
-            <Typography variant="h3" color="secondary" component="span">
+            <Typography variant='h3' color='secondary' component='span'>
               .me()
             </Typography>
           </Typography>
         </Box>
         <Box my={1}>
-          <Typography variant="body1" color="textSecondary">
-            Jaransanitwong 24 Banchanglor Bangkok noi, Bangkok
+          <Typography variant='body1' color='textSecondary'>
+            Salaya Phutthamonthon Nakhon pathom, Thailand
             <br />
             090-970-8163
             <br />
--- apps/frontend/src/components/Is.tsx
+++ apps/frontend/src/components/Is.tsx
@@ -5,28 +5,30 @@ import ScrollAnimation from 'react-animate-on-scroll';
 const Is = () => {
   return (
     <Box
-      minHeight="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
+      minHeight='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
     >
       <Box>
-        <ScrollAnimation animateIn="bounceInRight" animateOut="bounceOutLeft">
-          <Box display="flex">
-            <Typography variant="h3" color="textPrimary">
+        <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft'>
+          <Box display='flex'>
+            <Typography variant='h3' color='textPrimary'>
               raksit
-              <Typography variant="h3" color="secondary" component="span">
+              <Typography variant='h3' color='secondary' component='span'>
                 .is()
               </Typography>
             </Typography>
           </Box>
           <Box my={1}>
-            <Typography variant="h5" color="secondary">
+            <Typography variant='h5' color='secondary'>
               Raksit Nongbua
             </Typography>
           </Box>
-          <Typography variant="h6" color="textSecondary">
-            Web Developer ,
+          <Typography variant='h6' color='textSecondary'>
+            Front-end Developer,
+            <br />
+            Web Developer,
             <br /> Game Developer.
           </Typography>
         </ScrollAnimation>
