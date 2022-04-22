939c7011481d4a741e88a51456cec5398c58c993
Tan Raksit <tan.raksit@bitkub.com>
2022-04-22 09:26:55 +0700

    squash commit
    test commit squash reset HEAD
    
    squash commit
    
    test commit
    
    test squash commit rebase
    
    test 2
    
    test
    
    test commit
    
    squash commit
    
    commit all
    
    feat: edit git ignore
    
    test cm 8
    
    test 9
    
    sate

---
+++ apps/test.ts
@@ -0,0 +1,10 @@
+const test1 = '1';
+const test2 = '2';
+const test3 = '3';
+const test4 = '4';
+const test5 = '5';
+const test6 = '6';
+const test7 = '7';
+const test8 = '8';
+
+console.log(test7);
